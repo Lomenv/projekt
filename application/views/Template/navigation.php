008135556d1b72389415fb2ef39bbb890b7ca806
Lomenv <lomenv95@gmail.com>
2017-05-06 11:37:43 +0200

    vlozena fotka na uvodnu stranku

--- application/views/Template/navigation.php
+++ application/views/Template/navigation.php
@@ -44,13 +44,14 @@
 </nav>
 
 <header>
-    <div class="container">
-        <div class="intro-text">
+
+<br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
             <div class="intro-lead-in">Welcome To Our Studio!</div>
             <div class="intro-heading">It's Nice To Meet You</div>
             <a href="#services" class="page-scroll btn btn-xl">Tell Me More</a>
-        </div>
-    </div>
+
+
+
 </header>
 
 
